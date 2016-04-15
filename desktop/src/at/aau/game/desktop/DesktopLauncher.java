33db786a4c8bbb8ed260b65bf7842563366fe4b3
Herk0r <k.herkt@gmx.at>
2016-04-15 19:31:09 +0200

    libGDX start project

---
+++ desktop/src/at/aau/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.aau.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.aau.game.PixieSmack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PixieSmack(), config);
+	}
+}
