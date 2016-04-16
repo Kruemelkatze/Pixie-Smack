179675e1e37d692bf0ae91cc4cdba3df20ebfddc
Fabian <schoberf@gmx.net>
2016-04-16 14:12:17 +0200

    Fairy Animation

--- core/src/at/aau/game/Mechanics/Entities/FairyObject.java
+++ core/src/at/aau/game/Mechanics/Entities/FairyObject.java
@@ -16,51 +16,60 @@ import com.badlogic.gdx.math.Vector2;
  * @author Kevin Herkt
  */
 public class FairyObject extends MoveableObject {
-    private Animation idleAnimation;
-    private TextureRegion tempFrame;
+
+    private Animation leftAnimation;
+    private Animation rightAnimation;
+    private float animTime = 0;
+
     private int health = 1;
     private final Vector2 startPosition;
-	private Random random = new Random();
-	private boolean isBlackFairy = false;
+    private Random random = new Random();
+    private boolean isBlackFairy = false;
 
     static Vector2 SIZE = new Vector2(64, 64);
     private float speed = 3f;
+    private TextureRegion tempFrame;
 
     public FairyObject(Vector2 position, World world) {
         super(position, world, SIZE);
-		this.startPosition = this.position.cpy();
-		float rnd = random.nextFloat();
-		if (rnd < 0.33f) {
-			this.directionX = DirectionX.LEFT;
-			this.directionY = DirectionY.UP;
-			this.speed = 2.5f;
-		} else if (rnd < 0.66f) {
-			this.directionX = DirectionX.LEFT;
-			this.directionY = DirectionY.DOWN;
-			this.speed = 1.5f;
-		} else {
-			this.directionX = DirectionX.RIGHT;
-			this.directionY = DirectionY.DOWN;
-			this.speed = 1.0f;
-		}
-        this.idleAnimation = world.gameplayScreen.parentGame.getAnimator().loadAnimation(GameConstants.FAIRY_SPRITE_PATH, 0.0f, (int) this.size.x,
+        this.startPosition = this.position.cpy();
+        float rnd = random.nextFloat();
+        if (rnd < 0.33f) {
+            this.directionX = DirectionX.LEFT;
+            this.directionY = DirectionY.UP;
+            this.speed = 2.5f;
+        } else if (rnd < 0.66f) {
+            this.directionX = DirectionX.LEFT;
+            this.directionY = DirectionY.DOWN;
+            this.speed = 1.5f;
+        } else {
+            this.directionX = DirectionX.RIGHT;
+            this.directionY = DirectionY.DOWN;
+            this.speed = 1.0f;
+        }
+        this.leftAnimation = world.gameplayScreen.parentGame.getAnimator().loadAnimation("gameplay/pixie-left-anim.png", 0.3f, (int) this.size.x,
                 (int) this.size.y);
+        this.rightAnimation = world.gameplayScreen.parentGame.getAnimator().loadAnimation("gameplay/pixie-right-anim.png", 0.3f, (int) this.size.x,
+                (int) this.size.y);
+
+
     }
 
     @Override
     public void render(float delta, SpriteBatch spriteBatch) {
+        animTime += delta;
         if (this.isDead) {
             return;
         }
         switch (this.directionX) {
             case LEFT:
-                tempFrame = idleAnimation.getKeyFrame(animTime, true);
+                tempFrame = leftAnimation.getKeyFrame(animTime, true);
                 break;
             case RIGHT:
-                tempFrame = idleAnimation.getKeyFrame(animTime, true);
+                tempFrame = rightAnimation.getKeyFrame(animTime, true);
                 break;
             case STOP:
-                tempFrame = idleAnimation.getKeyFrame(animTime, true);
+                tempFrame = rightAnimation.getKeyFrame(animTime, true);
                 break;
             default:
                 break;
